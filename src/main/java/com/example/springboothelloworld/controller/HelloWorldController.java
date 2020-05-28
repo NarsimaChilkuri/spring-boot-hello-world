c792dc39a8030fb1d2c26e91bc655067fc5bc475
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2020-05-28 16:58:02 +0530

    pom file changed

--- src/main/java/com/example/springboothelloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/springboothelloworld/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ public class HelloWorldController
     @RequestMapping("/")  
     public String hello()   
     {  
-      return "Hello World " + System.getenv("ENV");  
+      return "Hello World " + System.getenv("STACK");  
     }  
 }  
