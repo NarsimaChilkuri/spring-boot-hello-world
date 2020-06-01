9c1f5bf08412469d757a16a0d59f63556da0202b
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2020-06-01 18:26:30 +0530

    added a helm chart

--- src/main/java/com/example/springboothelloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/springboothelloworld/controller/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.example.springboothelloworld.controller;
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RestController;  
-
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController  
 public class HelloWorldController   
 {  
+
+	@Autowired
+    private Environment env;
+
     @RequestMapping("/")  
     public String hello()   
     {  
-      return "Hello World " + System.getenv("STACK");  
+      return "Hello World " + env.getProperty("STACK");  
     }  
 }  
